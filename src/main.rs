370de84a404f856dd977904a79ae8c155eef3775
lj <jeahyoung@gmail.com>
2023-05-02 17:35:12 +0900

    second

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use bevy::{prelude::*, window::PrimaryWindow};
 
+pub const PLAYER_SPEED: f32 = 500.0;
+pub const PLAYER_SIZE: f32 = 64.0;
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
     .add_startup_system(spawn_camera)
     .add_startup_system(spawn_player)
+    .add_system(player_movement)
     .run();
 }
 
@@ -40,4 +43,45 @@ pub fn spawn_camera(
         transform: Transform::from_xyz(window.width()/2.0, window.height()/2.0, 0.0),
         ..default()
     });
+}
+
+
+
+pub fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut transform) = player_query.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A){
+            direction += Vec3::new(-1.0, 0.0, 0.0);
+        }
+        if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D){
+            direction += Vec3::new(1.0, 0.0, 0.0);
+        }
+        if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W){
+            direction += Vec3::new(0.0, 1.0, 0.0);
+        }
+        if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S){
+            direction += Vec3::new(0.0, -1.0, 0.0);
+        }
+
+        if direction.length() > 0.0 {
+            direction = direction.normalize();
+        }
+
+        transform.translation += direction * PLAYER_SPEED * time.delta_seconds();
+    }
+}
+
+pub fn confine_player_movement(
+    mut player_query: Query<&mut Transform, With<Player>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    if let Ok(mut player_transform) = player_query.get_single_mut() {
+        let window = window_query.get_single().unwrap();
+
+    }
 }
