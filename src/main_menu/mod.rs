351c36017f02a6d820abbd645e9c0bc187ca417f
lj <jeahyoung@gmail.com>
2023-05-04 16:37:10 +0900

    ui

--- src/main_menu/mod.rs
+++ src/main_menu/mod.rs
@@ -1,14 +1,34 @@
+mod components;
+mod styles;
+mod systems;
+
 use bevy::prelude::*;
+
+use crate::AppState;
+
+use self::systems::{layout::{spawn_main_menu, despawn_main_menu}, interactions::{interact_with_play_button, interact_with_quit_button}};
 
 pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
         app
-        .add_startup_system(main_menu);
+        //.add_startup_system(main_menu);
+        // OnEnter State System
+        .add_system(spawn_main_menu.in_schedule(OnEnter(AppState::MainMenu)))
+        .add_systems(
+            (
+                interact_with_play_button,
+                interact_with_quit_button
+            )
+            .in_set(OnUpdate(AppState::MainMenu))
+        )
+        // OnExit State System
+        .add_system(despawn_main_menu.in_schedule(OnExit(AppState::MainMenu)))
+        ;
     }
 }
 
-pub fn main_menu() {
-    println!("You are on the main menu.");
-}
+// pub fn main_menu() {
+//     println!("You are on the main menu.");
+// }
---
+++ src/main_menu/systems/interactions.rs
@@ -0,0 +1,43 @@
+use bevy::{prelude::*, app::AppExit};
+
+use crate::{main_menu::{components::{PlayButton, QuitButton}, styles::{PRESSED_BUTTON_COLOR, HOVERED_BUTTON_COLOR, NORMAL_BUTTON_COLOR}}, AppState};
+
+pub fn interact_with_play_button(
+    mut button_query: Query<(&Interaction, &mut BackgroundColor), (Changed<Interaction>, With<PlayButton>)>,
+    mut app_state_next_state: ResMut<NextState<AppState>>
+){
+    if let Ok((interaction, mut background_color)) = button_query.get_single_mut() {
+        match interaction {
+            Interaction::Clicked => {
+                *background_color = PRESSED_BUTTON_COLOR.into();
+                app_state_next_state.set(AppState::Game);
+            },
+            Interaction::Hovered => {
+                *background_color = HOVERED_BUTTON_COLOR.into();
+            },
+            Interaction::None => {
+                *background_color = NORMAL_BUTTON_COLOR.into();
+            },
+        }
+    }
+}
+
+pub fn interact_with_quit_button(
+    mut button_query: Query<(&Interaction, &mut BackgroundColor), (Changed<Interaction>, With<QuitButton>)>,
+    mut app_exit_event_writer: EventWriter<AppExit>
+){
+    if let Ok((interaction, mut background_color)) = button_query.get_single_mut() {
+        match interaction {
+            Interaction::Clicked => {
+                *background_color = PRESSED_BUTTON_COLOR.into();
+                app_exit_event_writer.send(AppExit);
+            },
+            Interaction::Hovered => {
+                *background_color = HOVERED_BUTTON_COLOR.into();
+            },
+            Interaction::None => {
+                *background_color = NORMAL_BUTTON_COLOR.into();
+            },
+        }
+    }
+}
